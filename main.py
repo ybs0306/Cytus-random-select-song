6d3e5de135e32b47fee19d217ac0973200904a71
kinoshitakenta <isu10403005A@go.isu.edu.tw>
2018-09-15 15:49:24 +0800

    修改成可以連續抽 以及增加排版

--- main.py
+++ main.py
@@ -6,5 +6,20 @@ with open("Cytus song list.txt", 'r') as f:
         items = line.strip('\n').split('\t',1)
         data.append(items)
 
-r = random.randint(0,210)
-print('{ID} - {song}'.format(ID=data[r][0],song=data[r][1]))
+loop = 1
+print("輸入要抽幾首:")
+
+while True:
+    try:
+        loop = int(input('\n'))
+        if loop >= 0:
+            for x in range(0,loop):
+                r = random.randint(0,210)
+                print('{ID} - {song}'.format(ID=data[r][0],song=data[r][1]))
+        else:
+            print("輸入格式錯囉")
+    except:
+        print("輸入格式錯囉")
+
+    if loop == 0:
+        break
