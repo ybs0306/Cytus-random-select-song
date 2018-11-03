e7bbb6c4f32ab4b7bfcd71219b970159cb664dfe
DingChen-Tsai <tdc@tdccc.com.tw>
2018-11-03 20:37:59 +0800

    make code more elegance.

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
+import os.path
 import random
 
-data = []
-with open("Cytus song list.txt", 'r') as f:
-    for line in f:
-        items = line.strip('\n').split('\t',1)
-        data.append(items)
+class select:
+    def __init__(self):
+        self.songlist = os.path.isfile('songlist.txt')
+        self.data = []
+        self.load()
 
-loop = 1
-print("輸入要抽幾首:")
-
-while True:
-    try:
-        loop = int(input('\n'))
-        if loop >= 0:
-            for x in range(0,loop):
-                r = random.randint(0,210)
-                print('{ID} - {song}'.format(ID=data[r][0],song=data[r][1]))
+    def load(self):
+        if self.songlist:
+            with open("songlist.txt", 'r', encoding='big5') as f:
+                for line in f:
+                    try:
+                        items = line.strip('\n').split('\t',1)
+                        self.data.append(items)
+                    except Exception as e:
+                        print('分析歌曲清單檔案錯誤\n{e}'.format(e=str(e)))
         else:
-            print("輸入格式錯囉")
-    except:
-        print("輸入格式錯囉")
+            print('找不到歌曲表')
 
-    if loop == 0:
-        break
+    def run(self):
+        while True:
+            input_num = input('輸入要抽幾首: ')
+            if input_num.isdigit() == True:
+                input_num = int(input_num)
+                if input_num >= 0:
+                    for x in range(0, input_num):
+                        r = random.randint(0, len(self.data))
+                        print('{id} - {song}'.format(
+                            id=self.data[r][0],
+                            song=self.data[r][1]
+                            ))
+                    break
+            else:
+                print("輸入格式錯囉")
+
+selectSong = select()
+selectSong.run()
