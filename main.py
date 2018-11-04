2e5fe5c3af34f0323b7ecee81e80c7c9348a292e
kinoshitakenta <isu10403005A@go.isu.edu.tw>
2018-11-04 11:21:09 +0800

    新增例外發生時break程式 取消連續抽機制

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__  = 'kinoshitakenta'
+__email__   = "ybs0306748@gmail.com"
+
 import os.path
 import random
 
@@ -15,13 +20,15 @@ class select:
                         items = line.strip('\n').split('\t',1)
                         self.data.append(items)
                     except Exception as e:
-                        print('分析歌曲清單檔案錯誤\n{e}'.format(e=str(e)))
+                        print('分析歌曲清單錯誤\n{e}'.format(e=str(e)))
+                        exit()
         else:
-            print('找不到歌曲表')
+            print('找不到歌曲表嗚')
+            exit()
 
     def run(self):
         while True:
-            input_num = input('輸入要抽幾首: ')
+            input_num = input('輸入要抽幾首 : ')
             if input_num.isdigit() == True:
                 input_num = int(input_num)
                 if input_num >= 0:
@@ -33,7 +40,7 @@ class select:
                             ))
                     break
             else:
-                print("輸入格式錯囉")
+                print("輸入的格式錯囉")
 
 selectSong = select()
 selectSong.run()
